cb071943c9ceb3b1efa1765ade151d45f0ceb7c9
Kristjan <krix@planet.ee>
2021-11-22 21:43:13 +0200

    commit

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -5,11 +5,24 @@ const { NotFoundError } = require('../errors');
 
 // get all posts
 const getAllPosts = async (req, res) => {
+  // PAGINATION
+  // page number - get from request query. Default page is 1
+  const page = Number(req.query.page) || 1;
+  // number of items for the page. Default is 10
+  const limit = Number(req.query.limit) || 5;
+  // calculate, how many pages to skip
+  const skip = (page - 1) * limit;
+
+  // If we want to get posts without pagination, we must remove skip and limit functions from inquiry
+
   // get all posts and sort them newest first
-  const posts = await Post.find().sort('-createdAt');
+  const posts = await Post.find().sort('-createdAt').skip(skip).limit(limit);
+
+  // get a count of all post documents
+  const count = await Post.countDocuments();
 
   // return result, which is posts array
-  res.status(StatusCodes.OK).json({ posts, count: posts.length });
+  res.status(StatusCodes.OK).json({ posts, count });
 };
 
 // get single post
